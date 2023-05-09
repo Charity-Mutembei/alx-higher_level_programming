3c7695a39f535ffc15da66c37a26b63777e6eba9
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-09 23:12:48 +0300

    Task5: 00..99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if (i < 10):
-        print("0{}".format(i), end=", ")
-    elif (10 < i <= 98):
-        print("{}".format(i), end=", ")
-    else:
-        print("{}".format(i), end="")
+for i in range(100):
+    if i < 10:
+        print("0", end="")
+    print("{:d}".format(i), end=", " if i < 99 else "")
