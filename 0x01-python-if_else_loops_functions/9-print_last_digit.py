f55beb9932b49c98cd2466a4bd54758437d82d51
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-10 00:08:14 +0300

    Task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    """
+    Prints the last digit of the given number and returns it.
+    """
+    # get the absolute value of the number to handle negative inputs
+    abs_number = abs(number)
+
+    # get the last digit of the absolute number using the modulo operator
+    last_digit = abs_number % 10
+
+    # print the last digit using a print statement
+    print(last_digit)
+
+    # return the last digit
+    return last_digit
