323dceeb67530ba26f91acdd2bd66f14c3379e37
Charity <charity.softwaredev@gmail.com>
2023-05-11 14:00:13 -0700

    Task 4: Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     names = dir(hidden_4)
     total = len(names)
 
-    for i in total:
+    for i in range(total):
         if names[i][:2] != '__':
             print(names[i])
