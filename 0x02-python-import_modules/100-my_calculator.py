083daa42df200196cc4388190d0edb202397b0f6
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-12 00:35:33 +0300

    Task 6: Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 import sys
+import sys
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    def print_usage():
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    
-    def perform_operation(a, operator, b):
+def print_usage():
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
 
-        if operator == '+':
-            result = add(a, b)
-        if operator == '-':
-            result = sub(a, b)
-        if operator == '*':
-            result = mul(a, b)
-        elif operator == '/':
-            result = div(a, b)
-        else:
-            print("Unknown Operator")
-        
-        print(f"{a} {operator} {b} = {result}")
+def perform_operation(a, operator, b):
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
+    print(f"{a} {operator} {b} = {result}")
+
+if __name__ == '__main__':
     if len(sys.argv) != 4:
         print_usage()
 
