45b0b73f02f687ee70f4a5979e92486a639b015a
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-15 01:24:45 +0300

    Task 5: Can you C me now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i != "C" and i != "C":
+        if i != "C" and i != "c":
             new_string += i
     return new_string
