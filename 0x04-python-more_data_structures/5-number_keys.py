2c684b4b2d5ba209fd7bdafb364fc0c69ce70749
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-18 02:00:36 +0300

    Task 5: Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    # Get the keys of the dictionary
+    keys = a_dictionary.keys()
+
+    # Calculate the number of keys
+    num_keys = len(keys)
+
+    return num_keys
