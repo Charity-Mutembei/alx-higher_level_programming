560026e6e1e04885a492b3c5a3cb445629a2bb2b
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-29 23:17:38 +0300

    Task 9: ByteCode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
+    answer = 0
 
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
+            else:
+                answer += (a ** b) / i
         except Exception:
-            raise
-        else:
-            result += (a ** b) / i
-
-    result += a + b
-
-    return result
+            answer = b + a
+            break
+    return answer
