a9fa28b602cecf2f8a45bcc152501e7dbe7c0218
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-29 23:10:26 +0300

    Task 8: Safe function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,8 +3,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
-    except Exception as e:
+    except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
