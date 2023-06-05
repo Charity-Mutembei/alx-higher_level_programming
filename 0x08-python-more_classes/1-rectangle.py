83618064abd2d65e7946455d6e605a1c2cc5cff0
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-06 02:24:47 +0300

    Task 0: Simple rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,19 +5,22 @@ class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
 
 
 class Rectangle:
+    """
+    class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
+    """
     def __init__(self, width=0, height=0):
-        """ Initialize rectangles """
+        """Initialize a Rectangle object with optional width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ Getter returns width """
+        """Retrieve the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Setter sets width if int > 0 """
+        """Set the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,12 +29,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """ Getter returns height """
+        """Retrieve the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Setter sets height if int > 0 """
+        """Set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
