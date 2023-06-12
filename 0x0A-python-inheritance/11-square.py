b46b24f88e9babb067eda5428272ddd7d66eba57
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-13 02:36:47 +0300

    Task 10, square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a class Square that inherits from Rectangle
+(9-rectangle.py). (task based on 10-square.py).
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """the constructor below intializes"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        """we make sure its positive"""
+        self.pos_size = size
+        """the area"""
+        super().__init__(self.pos_size, self.pos_size)
