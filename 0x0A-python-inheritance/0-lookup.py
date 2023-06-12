96c783b94332a9a3a2c3d3c284a02fe947430834
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-13 00:08:24 +0300

    operation complete

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,18 +6,6 @@ attributes and methods of an object:
 
 
 def lookup(obj):
-    # Create an empty list to store the attributes and methods
-    result = []
-
-    # Retrieve the list of all attributes and methods of the object
-    attributes_and_methods = dir(obj)
-
-    # Iterate over each attribute or method name
-    for attr in attributes_and_methods:
-        # Check if the attribute or method name does not start with '__'
-        if not attr.startswith('__'):
-            # Add the attribute or method name to the result list
-            result.append(attr)
-
-    # Return the final list of attributes and methods
-    return result
+    """function that returns the list of
+    avilable attributes and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/tests/0-lookup-tests.py
@@ -0,0 +1,32 @@
+import unittest
+import importlib.util
+
+spec = importlib.util.spec_from_file_location('lookup_module', '0-lookup.py')
+lookup_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(lookup_module)
+lookup = lookup_module.lookup
+
+
+class TestLookup(unittest.TestCase):
+    def test_lookup_empty_object(self):
+        class EmptyObject:
+            pass
+        obj = EmptyObject()
+        result = lookup(obj)
+        expected = []
+        self.assertEqual(result, expected)
+
+    def test_lookup_list_object(self):
+        obj = [1, 2, 3]
+        result = lookup(obj)
+        expected = ['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+        self.assertEqual(result, expected)
+
+    def test_lookup_string_object(self):
+        obj = "Hello, World!"
+        result = lookup(obj)
+        expected = ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
