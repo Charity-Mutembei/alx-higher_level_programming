7131b7d2fe5445523ba38d7bc087f9b74a07216e
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-13 02:18:49 +0300

    Task 9: my turn

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle (BaseGeometry):
+    """below is the constructor"""
+    def __init__(self, width, height):
+        """the width and height must be positive"""
+        self.integer_validator("width", width)
+        self.pos_width = width
+        """check the height"""
+        self.integer_validator("height", height)
+        self.pos_height = height
+
+    """the area() method must be implemented"""
+    def area(self):
+        "print"
+        string = "[Rectangle] {}/{}".format(self.pos_width, self.pos_height)
+        print(string)
+        "return"
+        return (self.pos_width*self.pos_height)
