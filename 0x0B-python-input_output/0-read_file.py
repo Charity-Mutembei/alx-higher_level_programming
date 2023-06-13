e40220dc9e1cfb8209b7845304f96000d6d22f31
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-14 01:54:35 +0300

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a function that reads a text file (UTF8) and prints it to stdout:
+"""
+
+
+def read_file(filename=""):
+    """
+    Open the file with the given filename in read
+    mode using the 'utf8' encoding
+    """
+    with open(filename, 'r', encoding='utf8') as file:
+        """
+        Read the entire contents of the file and store
+        it in the 'content' variable
+        """
+        content = file.read()
+
+        """ Print the content to stdout"""
+        print(content)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+README.md for the 0x0B-Python-input-output ptoject
