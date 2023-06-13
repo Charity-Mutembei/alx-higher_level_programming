2f41c923ff3cfa0404ca7b717d458860727a4653
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-13 03:01:15 +0300

    Task 12: rebel

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a class MyInt that inherits from int
+"""
+
+
+class MyInt(int):
+    """this class inherits from int"""
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
