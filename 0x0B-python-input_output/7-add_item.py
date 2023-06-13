cce7dc62869b52a1893bebb900810eef958f1c59
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-14 02:27:40 +0300

    --ammend

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,55 +3,33 @@
 a script that adds all arguments to
 a Python list, and then save them to a file:
 """
-import sys
-import json
+from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+"""remove the script name from the line arguments"""
+argv.pop(0)
 
-def save_to_json_file(data, filename):
-    """
-    Saves the given data as JSON to the specified file.
-
-    Args:
-        data: The data to be saved as JSON.
-        filename: The name of the file to save the data to.
-    """
-    with open(filename, 'w') as file:
-        json.dump(data, file)
-
-
-def load_from_json_file(filename):
-    """
-    Loads JSON data from the specified file and returns it.
-
-    Args:
-        filename: The name of the file to load JSON data from.
-
-    Returns:
-        The loaded JSON data.
-    """
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-"""
-Get the command line arguments excluding the script name
-"""
-arguments = sys.argv[1:]
 
 try:
-    """Attempt to load existing list from file"""
-    existing_list = load_from_json_file("add_item.json")
-    if existing_list is None:
-        existing_list = []
-
-    """Extend the existing list with the command line arguments"""
-    existing_list.extend(arguments)
-
-    """Save the updated list to the file"""
-    save_to_json_file(existing_list, "add_item.json")
+    """Attempts the load of exisitng lists from file"""
+    deserialized = load_from_json_file("add_item.json")
+    if deserialized is None:
+        """
+        If the file is empty is non-existent,
+        save command line arguments in a list
+        """
+        save_to_json_file(argv, "add_item.json")
+    else:
+        """
+        if the file exists and has data, extend the
+        existing list of command line arguments
+        """
+        deserialized.extend(argv)
+        save_to_json_file(deserialized, "add_item.json")
 except FileNotFoundError:
     """
-    If file not found, create a new file and
-    save the command line arguments as a list
+    if the file is not found then create new and save command
+    line argumets
     """
-    save_to_json_file(arguments, "add_item.json")
+    save_to_json_file(argv, "add_item.json")
