0f30ce978e1c754ed3f87b863a52019c656d4510
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-14 02:38:10 +0300

    --ammend

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -14,26 +14,12 @@ def class_to_json(obj):
         obj: An instance of a class.
 
     Returns:
-        A dictionary representation of the object suitable
-        for JSON serialization.
+        A dictionary representation of the object
+        suitable for JSON serialization.
     """
-    if isinstance(obj, dict):
-        """
-        If the object is a dictionary, recursively convert
-        its values to JSON-serializable dictionaries
-        """
-        return {k: class_to_json(v) for k, v in obj.items()}
-    elif isinstance(obj, list):
-        """
-        If the object is a list, recursively convert its
-        elements to JSON-serializable dictionaries
-        """
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, (str, int, bool)):
-        """
-        If the object is a string, integer, or boolean, return it directly
-        """
-        return obj
-    else:
-        """For other data types, raise an exception or handle them as needed"""
-        raise ValueError("Object of unsupported type encountered.")
+    i = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            i[key] = value
+
+    return i
