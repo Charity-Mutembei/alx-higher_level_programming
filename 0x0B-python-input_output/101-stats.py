57b794d05b4d0763c9b14baf88112d9853c4d8f3
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-14 03:00:54 +0300

    --ammend

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,9 +3,8 @@
 a script that reads stdin line by line and computes metrics
 """
 if __name__ == '__main__':
-        
-    import sys
 
+    import sys
 
     status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                     '403': 0, '404': 0, '405': 0, '500': 0}
