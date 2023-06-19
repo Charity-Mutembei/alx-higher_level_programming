ff566bf0d41865422c55a0c24f1b2acfe72f8bf4
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-19 20:56:50 +0300

    --ammend

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,7 @@ class Rectangle(Base):
     Reactangle class takes from base
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize the Rectangle object with width, height, x, y, and id"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -18,35 +19,44 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Getter method for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter method for width"""
         self.__width = value
 
     @property
     def height(self):
+        """Getter method for height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter method for height"""
         self.__height = value
 
     @property
     def x(self):
+        """Getter method for x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Setter method for x"""
         self.__x = value
 
     @property
     def y(self):
+        """Getter method for y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Setter method for y"""
         self.__y = value
 
     def area(self):
+        """Calculate and return the area of the rectangle"""
         return self.width * self.height
