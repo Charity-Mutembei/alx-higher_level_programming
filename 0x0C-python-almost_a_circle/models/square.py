c0186e6f16c8fe9f4e7a206c14c366d9e97f0410
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-19 21:42:53 +0300

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,14 +7,43 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    Square class that inherits from Rectangle
+    Square class inherits from Rectangle
     """
-
     def __init__(self, size, x=0, y=0, id=None):
-        """Call the superclass constructor with id, x, y,
-        size for width and height"""
+        """
+        Initialize Square instance
+        """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        Get the size attribute (same as width)
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size attribute (same as width)
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
-        """Return the string representation of the square"""
+        """
+        Return a string representation of the Square instance
+        """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """
+        Update the attributes of the Square instance
+        """
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
