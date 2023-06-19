9a24fc2f61c7144fcd37357bdc2a3b215947e8d0
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-19 22:19:56 +0300

    Task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,15 @@ class Base():
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Save a list of instances to a file in JSON format
+        """
+        filename = cls.__name__ + ".json"
+        json_list = []
+        if list_objs is not None:
+            json_list = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(json_list))
