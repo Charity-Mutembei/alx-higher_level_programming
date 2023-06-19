175c1ed9d9914fdcfefc637b4ac6fdefcc39a2ca
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-19 20:50:12 +0300

    add task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,11 +2,13 @@
 """
 Rectangle class that inherits from Base
 """
-
 from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    Reactangle class takes from base
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
