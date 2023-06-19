64303e43ea74c73f8b9adcff2c9baf3a387df994
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-19 22:49:47 +0300

    Task 20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,36 +18,6 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """
-        Convert a list of dictionaries to a JSON string representation
-        """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """
-        Save a list of instances to a file in JSON format
-        """
-        filename = cls.__name__ + ".json"
-        json_list = []
-        if list_objs is not None:
-            json_list = [obj.to_dictionary() for obj in list_objs]
-        with open(filename, "w") as file:
-            file.write(cls.to_json_string(json_list))
-
-    @staticmethod
-    def from_json_string(json_string):
-        """
-        Convert a JSON string representation to a list
-        """
-        if json_string is None or len(json_string) == 0:
-            return []
-        return json.loads(json_string)
-
     @classmethod
     def create(cls, **dictionary):
         """
