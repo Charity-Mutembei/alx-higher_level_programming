3535aa278bc0c7cefa14832cf577a27e5c360c13
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-07-20 02:24:41 +0300

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- script that creates the MySQL server user user_0d_1.
--- Check if the user user_0d_1 already exists
-SELECT COUNT(*) INTO @user_count FROM mysql.user WHERE User = 'user_0d_1';
 -- Create user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to the user user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost;
