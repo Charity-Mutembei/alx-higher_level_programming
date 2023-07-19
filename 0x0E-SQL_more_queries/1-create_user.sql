fe34891835a50672ee4a004e7984d4f3765fdc53
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-07-20 02:37:40 +0300

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- Create user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to the user user_0d_1
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost;
+GRANT ALL ON *.* TO user_0d_1@localhost;
