65aac551fe0429432d057f6db3cdcef098b34371
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-07-20 05:12:04 +0300

    task 5

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- script that lists all generes in the database
+SELECT tv_genres.name, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN hbtn_0d_tvshows_rate ON tv_show_genres.show_id = hbtn_0d_tvshows_rate.show_id
+GROUP BY tv_genres.id, tv_genres.name
+ORDER BY rating_sum DESC;
