881b48f6bd4ba47f0885f80484f52e6607f385bc
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-07-20 03:06:52 +0300

    This is task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,7 @@
 -- Script that creates the database hbtn_0d_2 and the user user_0d_2.
--- Creates
+-- Creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create User user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant select privilege in the database hbtn_0d_2
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
