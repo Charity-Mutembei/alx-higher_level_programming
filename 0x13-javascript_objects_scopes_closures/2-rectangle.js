3738e109c5445f22b6b082d8b78aba057bfbde9a
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-16 01:20:20 +0300

    edited: 2-rectangle.js Pushed at 2023-08-16 01:20:20

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       // If w or h is not a positive integer, create an empty object
       return this;
     }
