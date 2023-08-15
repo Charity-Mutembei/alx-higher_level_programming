fdcb8eb59764fe8d2f59158256d399f3eaec740f
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-16 02:24:16 +0300

    created:  Pushed at 2023-08-16 02:24:16

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+if (args.length !== 3) {
+  console.error('Usage: ./102-concat.js <file1> <file2> <destination>');
+  process.exit(1);
+}
+
+const file1Path = args[0];
+const file2Path = args[1];
+const destinationPath = args[2];
+
+const file1Content = fs.readFileSync(file1Path, 'utf8');
+const file2Content = fs.readFileSync(file2Path, 'utf8');
+
+const concatenatedContent = file1Content + file2Content;
+
+fs.writeFileSync(destinationPath, concatenatedContent, 'utf8');
