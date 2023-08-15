ffddd09539f5ff3806473c89316004e761cdd437
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-15 03:00:23 +0300

    created:  Pushed at 2023-08-15 03:00:23

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv[2];
+if (isNaN(args)) {
+  console.log('Missing size');
+} else {
+  number = parseInt(args);
+  for (let i = 0; i < number; i++) {
+    let row = '';
+    for (let j = 0; j < number; j++) {
+      row += '* ';
+    }
+    console.log(row);
+  }
+}
