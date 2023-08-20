6912d56b8aec16afca59abba153d11fe139739fb
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-21 02:38:44 +0300

    edited: 5-filter_cities.py Pushed at 2023-08-21 02:38:44

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -30,7 +30,7 @@ def main():
         SELECT cities.id, cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
+        WHERE states.name LIKE BINARY %s
         ORDER BY cities.id ASC
     """
 
