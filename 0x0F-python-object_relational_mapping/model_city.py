07c467ea2e91f181e78d22f191ac03e77b64866f
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-21 03:15:25 +0300

    2 file(s)  created:  Pushed at 2023-08-21 03:15:25

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module for defining the City class.
+"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+# Define the City class
+class City(Base):
+    """
+    Represents a city in the hbtn_0e_14_usa database.
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
