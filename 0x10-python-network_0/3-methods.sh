2f2d6bdc44c937d2d16ccfade936b6553ddc1aec
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-31 23:57:15 +0300

    edited: 3-methods.sh Pushed at 2023-08-31 23:57:15

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#checking the HTTP methods the server can accept
+#checking the HTTP methods the server can accept nn
 curl -sI "$1" | awk '/Allow:/ { gsub(/^[ \t]+|[ \t]+$/, ""); print substr($0, index($0,$2)) }'
