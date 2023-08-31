6779f27b2f5acf4a4037e1b9d6992492f7f82d0f
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-31 23:13:56 +0300

    edited: 0-body_size.sh Pushed at 2023-08-31 23:13:56

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #this is a script that takes in the url,
-#sends a request to that url, 
-#and displays the size of the body response in bytes
-# Check if an argument (URL) is provided
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
-# Use curl to send a request and pipe the response to wc -c to count bytes
 byte_size=$(curl -s "$1" | wc -c)
-# Display the byte size of the HTTP response
 echo "$byte_size"
 exit 0
