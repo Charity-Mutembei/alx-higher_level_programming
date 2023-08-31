f10e6b5db391632ef02b94b3270f0424195093e2
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-31 23:16:26 +0300

    edited: 0-body_size.sh Pushed at 2023-08-31 23:16:26

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #this is a script that takes in the url,
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-byte_size=$(curl -s "$1" | wc -c)
-echo "$byte_size"
-exit 0
+curl -s "$1" | wc -c
