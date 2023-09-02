ec9b807e2c917c7b07903efe4a82902c6f642b68
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-02 04:03:38 +0300

    edited: 3-error_code.py Pushed at 2023-09-02 04:03:38

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,24 +6,23 @@ response decoded in utf-8
 """
 
 
-import urllib.request
-import urllib.parse
 import sys
+from urllib import request, error
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+
+def fetch_url_content(url):
+    try:
+        with request.urlopen(url) as response:
+            return response.read().decode('UTF-8')
+    except error.HTTPError as e:
+        return f'Error code: {e.code}'
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
         sys.exit(1)
 
     url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    try:
-        with urllib.request.urlopen(req) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.URLError as e:
-        print("Error sending POST request:", e)
+    content = fetch_url_content(url)
+    print(content)
