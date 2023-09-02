6aedad8aa3083ed5151f87897890d832ed7d075a
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-02 03:48:30 +0300

    edited: 0-hbtn_status.py Pushed at 2023-09-02 03:48:30

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,11 +7,9 @@ import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print(f"Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-except urllib.error.URLError as e:
-    print(f"Error fetching URL: {e}")
+
+with urllib.request.urlopen(url) as response:
+    body = response.read().decode('utf-8')
+    print(f"Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
