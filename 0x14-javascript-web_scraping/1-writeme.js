ec5a0a8a8c116ae6d07c1108a5abafe217ff6d82
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-26 17:45:22 +0300

    created:  3 file(s) deleted: 0-readme.js README.md cisfun Pushed at ...

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if the correct number of arguments is provided
+if (process.argv.length !== 4) {
+  console.error('Usage: node 1-writeme.js <file-path> "<string-to-write>"');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+// Write the string to the file in utf-8
+fs.writeFile(filePath, stringToWrite, 'utf-8', (error) => {
+  if (error) {
+    // Handle the error by printing the error object
+    console.error(error);
+  } else {
+    console.log(`Successfully wrote "${stringToWrite}" to ${filePath}`);
+  }
+});
